8419da7b78c8b283919f605efc6d2fb0c7e818a2
ULYAPE2002 <ulyaescobar@gmail.com>
2023-07-23 17:34:36 +0700

    add auth controller and route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    // Fungsi untuk menampilkan halaman login
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    // Fungsi untuk memproses data login
+    public function processLogin(Request $request)
+    {
+        // Validasi input login
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Coba untuk melakukan login
+        if (Auth::attempt($request->only('email', 'password'))) {
+            // Jika login berhasil, redirect ke halaman dashboard atau halaman lain yang Anda inginkan
+            return redirect()->route('dashboard');
+        } else {
+            // Jika login gagal, kembali ke halaman login dengan pesan error
+            return redirect()->back()->with('error', 'Email atau password salah.');
+        }
+    }
+
+    // Fungsi untuk menampilkan halaman registrasi
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    // Fungsi untuk memproses data registrasi
+    public function processRegistration(Request $request)
+    {
+        // Validasi input registrasi
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        // Simpan data user baru ke database
+        $user = new \App\Models\User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        // Redirect ke halaman login dengan pesan sukses
+        return redirect()->route('login')->with('success', 'Registrasi berhasil. Silakan login.');
+    }
+
+    // Fungsi untuk logout
+    public function logout()
+    {
+        Auth::logout();
+
+        // Redirect ke halaman login dengan pesan sukses
+        return redirect()->route('login')->with('success', 'Anda telah berhasil logout.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'processLogin']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'processRegistration']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
